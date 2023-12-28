c9d0dbb4d17c1c236b43f5d10a3ec364b4403dc7
Jannik Steenken <jannik.steenken@capgemini.com>
2023-12-28 16:26:21 +0100

    fix: repair build and fix tests

---
+++ src/test/java/io/github/onecx/product/store/bff/rs/ProductsRestControllerIT.java
@@ -0,0 +1,7 @@
+package io.github.onecx.product.store.bff.rs;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ProductsRestControllerIT extends ProductsRestControllerTest {
+}

bab2c95493b116151aeb5280fd233b43ac4e6da1
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-02-24 16:15:30 +0100

    feat: delete image endpoint

--- src/test/java/org/tkit/onecx/product/store/bff/rs/ImagesRestControllerTest.java
+++ src/test/java/org/tkit/onecx/product/store/bff/rs/ImagesRestControllerTest.java
@@ -347,6 +347,27 @@ class ImagesRestControllerTest extends AbstractTest {
     }
 
     @Test
+    void deleteImage() {
+        var refId = "productName";
+        mockServerClient
+                .when(request().withPath("/internal/images/" + refId + "/" + RefType.LOGO).withMethod(HttpMethod.DELETE))
+                .withPriority(100)
+                .withId(MOCK_ID)
+                .respond(httpRequest -> response().withStatusCode(NO_CONTENT.getStatusCode()));
+
+        given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .pathParam("refId", refId)
+                .pathParam("refType", RefTypeDTO.LOGO)
+                .when()
+                .delete()
+                .then()
+                .statusCode(NO_CONTENT.getStatusCode());
+    }
+
+    @Test
     void testMaxUploadSize() {
 
         var refId = "themeMaxUpload";

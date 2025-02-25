51a5ad97c14f535922d3a9470d77eee79164fdeb
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2025-02-25 06:20:23 +0100

    Feature/merge upload update image (#114)
    * feat: merged update upload to one endpoint
    
    * feat: delete image endpoint

--- src/test/java/org/tkit/onecx/product/store/bff/rs/ImagesRestControllerTest.java
+++ src/test/java/org/tkit/onecx/product/store/bff/rs/ImagesRestControllerTest.java
@@ -320,7 +320,7 @@ class ImagesRestControllerTest extends AbstractTest {
         imageInfoDTO.setId("11-111");
 
         mockServerClient
-                .when(request().withPath("/internal/images/" + refId + "/" + RefType.LOGO).withMethod(HttpMethod.PUT))
+                .when(request().withPath("/internal/images/" + refId + "/" + RefType.LOGO).withMethod(HttpMethod.POST))
                 .withPriority(100)
                 .withId(MOCK_ID)
                 .respond(httpRequest -> response().withStatusCode(CREATED.getStatusCode())
@@ -336,7 +336,7 @@ class ImagesRestControllerTest extends AbstractTest {
                 .when()
                 .body(FILE)
                 .contentType(MEDIA_TYPE_IMAGE_PNG)
-                .put()
+                .post()
                 .then()
                 .statusCode(CREATED.getStatusCode())
                 .contentType(APPLICATION_JSON)
@@ -347,32 +347,24 @@ class ImagesRestControllerTest extends AbstractTest {
     }
 
     @Test
-    void updateImage_shouldReturnNotFound() {
-
-        var refId = "themeName";
-
-        ImageInfoDTO imageInfoDTO = new ImageInfoDTO();
-        imageInfoDTO.setId("11-111");
-
+    void deleteImage() {
+        var refId = "productName";
         mockServerClient
-                .when(request().withPath("/internal/images/" + refId + "/" + RefType.LOGO).withMethod(HttpMethod.PUT))
+                .when(request().withPath("/internal/images/" + refId + "/" + RefType.LOGO).withMethod(HttpMethod.DELETE))
                 .withPriority(100)
                 .withId(MOCK_ID)
-                .respond(httpRequest -> response().withStatusCode(NOT_FOUND.getStatusCode()));
+                .respond(httpRequest -> response().withStatusCode(NO_CONTENT.getStatusCode()));
 
-        var res = given()
+        given()
                 .when()
                 .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
                 .header(APM_HEADER_PARAM, ADMIN)
                 .pathParam("refId", refId)
                 .pathParam("refType", RefTypeDTO.LOGO)
                 .when()
-                .body(FILE)
-                .contentType(MEDIA_TYPE_IMAGE_PNG)
-                .put()
+                .delete()
                 .then()
-                .statusCode(NOT_FOUND.getStatusCode());
-        Assertions.assertNotNull(res);
+                .statusCode(NO_CONTENT.getStatusCode());
     }
 
     @Test
